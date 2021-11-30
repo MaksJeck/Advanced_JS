ca21270f965bf17935f409ac8b9274fd4f98ce8f
MaksJeck <maks-jeck@mail.ru>
2021-11-30 21:25:53 +0600

    lesson-2

--- script.js
+++ script.js
@@ -6,27 +6,51 @@ const goods = [
   { title: 'Shoes', price: 250 },
 ];
 
-/**
- * Функция возвращает разметку в html со списком товаров и цен
- * @param {} param0 
- * @returns 
- */
-const renderGoodsItem = ({ title = 'Shirt', price = 100 }) =>
-  `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`
 
-/**
- * Функция собирает все товары в один список и записывает в контейнер .goods-list
- * @param {*} list 
- */
-const renderGoodsList = (list) => {
-  // после каждого товара добавляется запятая - это происходит, потому-что
-  // выводится список свойств, из массива, и они автоматом разделяются запятой
-  // Чтобы избавиться от запятой, используем оператор соединения "join" с пустой
-  // строкой (' ');
-  let goodsList = list.map((item) => renderGoodsItem(item)).join('');
-  document.querySelector('.goods-list').innerHTML = goodsList;
+class GoodsItem {
+  constructor({ title = 'Shirt', price = 100 }) {
+    this.title = title;
+    this.price = price;
+  }
+  render() {
+    return `
+      <div class="goods-item">
+        <h3>${this.title}</h3>
+        <p>${this.price}</p>
+      </div>
+    `
+  }
+}
+
+class GoodsList {
+  constructor() {
+    this.goods = goods;
+  }
+  getSum() {
+    return this.goods.reduce((prev, { price }) => prev + price, 0);
+  }
+  render() {
+    const _goods = [...this.goods];
+    const _goodsItems = _goods.map((item) => {
+      const goodsItem = new GoodsItem(item);
+      return goodsItem.render();
+    })
+    document.querySelector('.goods-list').innerHTML = _goodsItems.join('');
+  }
+}
+
+class Basket {
+  setVision() { }
+  render() { }
+}
+
+class BasketItem {
+  setCount() { }
+  deleteItem() { }
+  render() { }
 }
 
 onload = () => {
-  renderGoodsList(goods);
+  const goodsList = new GoodsList();
+  goodsList.render();
 }
